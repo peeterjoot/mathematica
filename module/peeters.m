9051cb295406b721a5e197f21b8a5632caaaf0ac
Peeter Joot <peeter.joot@gmail.com>
2013-12-13 10:42:12 -0500

    use forward slashes.  Experiment with smaller 3D graphics plot export methods.
    modified:   module/peeters.m
    modified:   phy487/ps8e.nb
    modified:   phy487/qmSolidsPs8dContourPlot.nb

--- module/peeters.m
+++ module/peeters.m
@@ -6,7 +6,7 @@ Begin["peeters`"]
 ClearAll[exportForLatex, setGitDir];
 
 setGitDir::usage = "Peeter's home laptop: set working dir relative to physicsplay/ (like figures\phy487)" ;
-setGitDir[where_] := SetDirectory[ "C:\\Users\\Peeter\\cygwin_home\\physicsplay\\" <> where ] ;
+setGitDir[where_] := SetDirectory[ "C:/Users/Peeter/cygwin_home/physicsplay/" <> where ] ;
 
 exportForLatex::usage = "peeters`exportForLatex[ filename, image] ::This seems to be the most compact way to export for latex that still retains good resolution.  Use the epstopdf program to convert the resulting .eps file.  Also generate a .png for wordpress posts at the same time. \[IndentingNewLine]Note that the .png displays with some software having a checkerboard background, but that doesn't show up in the eventual web view.\[IndentingNewLine]\[IndentingNewLine]This png file is generated with a different basename so that latex includegraphics doesn't find it." ;
 
