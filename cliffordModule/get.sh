950c62ece160745194c3d188e1f89640664c119d
Peeter Joot <peeter.joot@gmail.com>
2014-02-20 11:23:19 -0500

    new file:   cliffordModule/GNUmakefile new file:   cliffordModule/UserGuide.nb new file:   cliffordModule/get.sh new file:   cliffordModule/tryClifford.nb

---
+++ cliffordModule/get.sh
@@ -0,0 +1,8 @@
+#wget http://www.fata.unam.mx/investigacion/departamentos/nanotec/aragon/software/Clifford.nb
+#wget http://www.fata.unam.mx/investigacion/departamentos/nanotec/aragon/software/UserGuide.pdf
+#wget http://www.fata.unam.mx/investigacion/departamentos/nanotec/aragon/software/ZhangEtAl.zip
+#wget http://www.fata.unam.mx/investigacion/departamentos/nanotec/aragon/software/clifford.m
+#wget http://arxiv.org/pdf/0810.2412v1.pdf
+
+# have modified:
+#wget http://www.fata.unam.mx/investigacion/departamentos/nanotec/aragon/software/UserGuide.nb
